cccc33d42601916bc32a2ebe86bc9e47856b008c
linlifeng <linlifeng@gmail.com>
2020-08-08 17:27:46 -0700

    fixed the rotation logic where the exif tag is not available for some pictures

--- app.py
+++ app.py
@@ -137,14 +137,14 @@ def upload_and_save_image_file(request):
     # pillow will mess up the photo rotation if this is not down:
     for orientation in ExifTags.TAGS.keys():
         if ExifTags.TAGS[orientation] == 'Orientation': break
-    exif = dict(image._getexif().items())
-
-    if exif[orientation] == 3:
-        image = image.rotate(180, expand=True)
-    elif exif[orientation] == 6:
-        image = image.rotate(270, expand=True)
-    elif exif[orientation] == 8:
-        image = image.rotate(90, expand=True)
+    if image._getexif():
+        exif = dict(image._getexif().items())
+        if exif[orientation] == 3:
+            image = image.rotate(180, expand=True)
+        elif exif[orientation] == 6:
+            image = image.rotate(270, expand=True)
+        elif exif[orientation] == 8:
+            image = image.rotate(90, expand=True)
     # end restoring rotation issue
 
     image.thumbnail(MAX_SIZE)
--- static/photos/thumbnail.py
+++ static/photos/thumbnail.py
@@ -1,5 +1,5 @@
 from sys import argv
-from PIL import Image
+from PIL import Image, ExifTags
 import os
 
 filename = argv[1]
@@ -10,15 +10,16 @@ THUMB_SIZE = (200, 200)
 
 # pillow will mess up the photo rotation if this is not down:
 for orientation in ExifTags.TAGS.keys():
-if ExifTags.TAGS[orientation] == 'Orientation': break
-exif = dict(image._getexif().items())
+	if ExifTags.TAGS[orientation] == 'Orientation': break
 
-if exif[orientation] == 3:
-image = image.rotate(180, expand=True)
-elif exif[orientation] == 6:
-image = image.rotate(270, expand=True)
-elif exif[orientation] == 8:
-image = image.rotate(90, expand=True)
+if image._getexif():
+	exif = dict(image._getexif().items())
+	if exif[orientation] == 3:
+		image = image.rotate(180, expand=True)
+	elif exif[orientation] == 6:
+		image = image.rotate(270, expand=True)
+	elif exif[orientation] == 8:
+		image = image.rotate(90, expand=True)
 # end restoring rotation issue
 
 image.thumbnail(MAX_SIZE)
