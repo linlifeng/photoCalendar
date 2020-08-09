784d2e8327e3716b770764a3a1f3db38c7a32ada
linlifeng <linlifeng@gmail.com>
2020-08-08 17:19:36 -0700

    fixed pillow rotating photo bug

--- app.py
+++ app.py
@@ -129,10 +129,24 @@ def upload_and_save_image_file(request):
 
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     # the photo can be very big. Downsize to save space
-    from PIL import Image
+    from PIL import Image, ExifTags
     image = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     MAX_SIZE = (1200, 800)
     THUMB_SIZE = (200, 200)
+
+    # pillow will mess up the photo rotation if this is not down:
+    for orientation in ExifTags.TAGS.keys():
+        if ExifTags.TAGS[orientation] == 'Orientation': break
+    exif = dict(image._getexif().items())
+
+    if exif[orientation] == 3:
+        image = image.rotate(180, expand=True)
+    elif exif[orientation] == 6:
+        image = image.rotate(270, expand=True)
+    elif exif[orientation] == 8:
+        image = image.rotate(90, expand=True)
+    # end restoring rotation issue
+
     image.thumbnail(MAX_SIZE)
     image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     # create thumbnail
--- static/photos/thumbnail.py
+++ static/photos/thumbnail.py
@@ -7,6 +7,20 @@ thumbname = 'thumb-' + os.path.basename(filename)
 image = Image.open(filename)
 MAX_SIZE = (1200, 800)
 THUMB_SIZE = (200, 200)
+
+# pillow will mess up the photo rotation if this is not down:
+for orientation in ExifTags.TAGS.keys():
+if ExifTags.TAGS[orientation] == 'Orientation': break
+exif = dict(image._getexif().items())
+
+if exif[orientation] == 3:
+image = image.rotate(180, expand=True)
+elif exif[orientation] == 6:
+image = image.rotate(270, expand=True)
+elif exif[orientation] == 8:
+image = image.rotate(90, expand=True)
+# end restoring rotation issue
+
 image.thumbnail(MAX_SIZE)
 image.save(filename)
 image.thumbnail(THUMB_SIZE)
